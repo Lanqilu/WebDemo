eac37a60ebcdb783193273401197b1a0bb702a3d
Lanqilu <885240677@qq.com>
2020-12-02 16:28:31 +0800

    Initial commit

---
+++ JavaScript/JSDemo/js/js1.js
@@ -0,0 +1,10 @@
+document.write("<b>加粗</b>");
+
+var i = 10;
+var j = 10;
+var m = i + j;
+document.write(m + "****" + typeof (m));
+
+i = "abc";
+document.write(i + "****" + typeof (i));
+i = NaN;
---
+++ JavaScript/text.js
@@ -0,0 +1,4 @@
+var message = "hello",found = false;
+console.log(typeof message)
+console.log(typeof(found))
+console.log(typeof null)
